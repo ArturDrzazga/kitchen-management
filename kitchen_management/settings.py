dc805f620727be9cadad136ad496b8bde8ed2788
Artur Drzazga <artur.drzazga03@gmail.com>
2025-11-10 14:48:56 +0100

    update settings & requirements.txt

--- kitchen_management/settings.py
+++ kitchen_management/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-uqvsbq=5c-4mhxe*bt5dml^dyg^(7#!o%m*z@#k$2u!r1w3(j!"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-uqvsbq=5c-4mhxe*bt5dml^dyg^(7#!o%m*z@#k$2u!r1w3(j!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
